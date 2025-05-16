a924e2acd758ee156b278c7673d9a1860fb9121f
jasonjordan <jason.jordan@gmail.com>
2025-05-17 07:05:45 +0800

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,12 @@
 const config = {
-    apiKey: "AIzaSyAVGQi8Shm9nxyDKupuQCYMPT6PpRlSlBU",
-    authDomain: "tripchat2.firebaseapp.com",
-    projectId: "tripchat2",
-    storageBucket: "tripchat2.firebasestorage.app",
-    messagingSenderId: "565535776882",
-    appId: "1:565535776882:web:bffaf1fc7506470e5e22eb",
-    measurementId: "G-WED154QNSP",
-    databaseURL: "https://tripchat2-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: "AIzaSyAVGQi8Shm9nxyDKupuQCYMPT6PpRlSlBU",
+  authDomain: "tripchat2.firebaseapp.com",
+  databaseURL: "https://tripchat2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tripchat2",
+  storageBucket: "tripchat2.firebasestorage.app",
+  messagingSenderId: "565535776882",
+  appId: "1:565535776882:web:e4914cee470759d95e22eb",
+  measurementId: "G-JZ8HJR4DS8"
 };
 
 firebase.initializeApp(config)
