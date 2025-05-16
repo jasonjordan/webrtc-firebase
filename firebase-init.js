931ce31ceb19f2eb2ece6a5881ce8d33feb4fe36
jasonjordan <jason.jordan@gmail.com>
2025-05-17 07:01:53 +0800

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -6,7 +6,7 @@ const config = {
     messagingSenderId: "565535776882",
     appId: "1:565535776882:web:bffaf1fc7506470e5e22eb",
     measurementId: "G-WED154QNSP",
-    databaseUrl: "https://tripchat2-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://tripchat2-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(config)
